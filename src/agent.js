7ec3ae621308059edde8173c5dbf5e7f42499eda
Kamal <93322430+kamalsq@users.noreply.github.com>
2021-11-16 16:49:12 +0530

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'http://node-js-backend-api:3000/api';
+const API_ROOT = 'http://node-js-backend-api/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
